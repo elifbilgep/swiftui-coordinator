43d914188049fa7dc8d343fe64bb43b779d9262c
Elif Bilge Parlak <elf.prlk64@gmail.com>
2025-12-14 23:54:20 +0300

    Initial Commit

---
+++ CoordinatorDeneme/CoordinatorDenemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinatorDenemeApp.swift
+//  CoordinatorDeneme
+//
+//  Created by Elif Bilge Parlak on 14.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct CoordinatorDenemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
